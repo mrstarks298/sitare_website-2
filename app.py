b4a51aee89af6b644d51481b9a362ef7a4cf2812
mrstarks298 <143446795+mrstarks298@users.noreply.github.com>
2024-02-01 15:21:12 +0000

    Added api and dynamic data

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask,render_template
 #create flask aplication
 app = Flask(__name__)
-JOBS={
+#crete python list
+JOBS = [
+{
   'id':1,
   'title':'Asistent prof. Python ','location':'Indore, India','Salary':'Rs. 100000'
-}
+},
 {
-  'id':2,'title':'Data Science Prof. ','location':'Indore','India','Salary':'Rs.150000'
-}
+  'id':2,'title':'Data Science Prof.','location':'Indore, India','Salary':'Rs.150000'
+},
 {
-'id':3,'title':'Machine Learning Prof.','location':'Lucknow ','India','Salary':'Rs.200000'
+'id':3,'title':'Machine Learning Prof.','location':'Lucknow ,India','Salary':'Rs.200000'
 }
+]
+
 
 
 
@@ -18,7 +22,11 @@ JOBS={
 #inside the rout the path of the page will mention
 @app.route('/')
 def hellow_world():
-  return render_template('index.html',jobs=JOBS)
+    return render_template('index.html', job=JOBS)
+#we can show data in json formate by:
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
